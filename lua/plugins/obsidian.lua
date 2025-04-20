7660b2c19984b8aaba6c854d73873998304b32b7
vtpb <vascotpbatista@gmail.com>
2025-04-20 18:44:58 +0100

    fix(obsidian.nvim): update source of community repo

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -2,7 +2,8 @@ if true then
   return {}
 end
 return {
-  "epwalsh/obsidian.nvim",
+  -- "epwalsh/obsidian.nvim",
+  "obsidian-nvim/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
   lazy = true,
   -- ft = "markdown",
